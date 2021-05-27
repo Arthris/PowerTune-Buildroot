0887801ae30f4a341974852f4709471dcab8b364
Arthris <cverneuil@gmail.com>
2021-05-27 11:48:10 -0400

    Fixed previous commit that broke things

---
+++ board/raspberrypi/rootfs_overlay/home/pi/PowerTune/updatedaemons.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "Fetching latest Daemons"
+cd /home/pi/src
+git pull
+cd
+echo "Killing all  Daemons"
+sudo pkill OBD
+sudo pkill Haltechd
+sudo pkill Consult
+sudo pkill Apexid 
+sudo pkill Microtechd 
+sudo pkill Linkd
+sudo pkill Hondatad
+sudo pkill AdaptronicCANd
+sudo pkill AEMV2d
+sudo pkill BRZFRS86d
+sudo pkill EMUCANd
+sudo pkill M800ADLSet1d
+sudo pkill MotecM1d
+sudo pkill AudiB7d
+sudo pkill AudiB8d
+sudo pkill Emtrond
+sudo pkill Holleyd
+sudo pkill Consultd
+sudo pkill FordBarraFGMK1CAN
+echo "Updating Daemons"
+sudo cp -r /home/pi/src/daemons/. /home/pi/daemons/
+
